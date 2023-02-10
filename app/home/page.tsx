df50ba69b6631a8b634ef9720c8b37ac17c11a5f
schaldach <gabimorgado0311@gmail.com>
2023-02-10 13:21:50 -0300

    testando queries

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,29 @@
-import React from "react";
-import Link from "next/link";
+"use client";
 
-function Home({user}:any) {
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { collection, query, Timestamp, where } from 'firebase/firestore'
+import { db } from "../../lib/Firebase";
+import { auth } from "../../lib/Firebase";
+
+function Home() {
+  const [user, setUser] = useState<any>(null)
+  const usersRef = collection(db, "users");
+
+    useEffect(() => {
+        const currentUser = auth.currentUser
+        if(currentUser){
+            const q = query(usersRef, where("user_id", "==", currentUser.uid))
+            setUser(q)
+        }
+  }, [])
+  
     return (
-        <div>
-            <Link href='/profile'>meu perfil</Link>
-        </div>
-    );
+    <div>
+      {user?<div>{user.user_name}</div>:''}
+      <Link href="/profile">meu perfil</Link>
+    </div>
+  );
 }
 
 export default Home;
